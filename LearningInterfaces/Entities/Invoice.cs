eb468350fe722cc52161803b2bbf7c02c7e48600
Luiz Huallpa <luiz.huallpa@gmail.com>
2022-08-29 13:30:06 -0300

    Add project files.

---
+++ LearningInterfaces/Entities/Invoice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningInterfaces.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return $"Basic payment: {BasicPayment:F2} \nTax: {Tax:F2} \nTotal payment: {TotalPayment:F2}" ;
+        }
+
+    }
+}
